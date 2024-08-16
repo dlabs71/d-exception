770db829f417b803ab09949df8a4691c8c14013a
dlabs71 <danila.a.ivanov@d-labs.ru>
2024-08-16 18:49:15 +0300

    Initial commit

---
+++ src/test/java/ru/dlabs71/library/exception/MainTest.java
@@ -0,0 +1,13 @@
+package ru.dlabs71.library.exception;
+
+/**
+ * <p>
+ * <div><strong>Project name:</strong> d-exception </div>
+ * <div><strong>Creation date:</strong> 2024-08-16 </div>
+ * </p>
+ *
+ * @author Ivanov Danila
+ * @since 1.0.0
+ */
+public class MainTest {
+}
